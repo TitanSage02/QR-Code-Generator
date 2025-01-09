4686b2c16d274d2eeb5f72c72e03f70c04c1616f
TitanSage02 <eayiwahoun@gmail.com>
2025-01-09 11:15:00 +0100

    Remove rectangle drawing from QR code generation

--- main.py
+++ main.py
@@ -20,14 +20,6 @@ img = qr.make_image(fill='black', back_color='white')
 draw = ImageDraw.Draw(img)
 width, height = img.size
 
-# Taille du rectangle
-rect_size = 20
-
-# Ajouter des rectangles dans trois des quatre coins
-draw.rectangle([0, 0, rect_size, rect_size], fill='black')  # Coin supérieur gauche
-draw.rectangle([width - rect_size, 0, width, rect_size], fill='black')  # Coin supérieur droit
-draw.rectangle([0, height - rect_size, rect_size, height], fill='black')  # Coin inférieur gauche
-
 
 # Afficher l'image pour vérifier
 img.show()
